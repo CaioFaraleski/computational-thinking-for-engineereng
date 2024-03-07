cd37ca5cb66f2ec50b84a7913cb9932c4314eaf6
Caio <caiotadeu_2012@hotmail.com>
2024-03-06 21:29:26 -0300

    test5

--- primeiraAula.py
+++ primeiraAula.py
@@ -1,1 +1,16 @@
-# 
+a = 3
+b = 5
+c = 7
+
+
+print("Caio tadeu Da Silva Faraleski")
+print(2 * a * 3 * b)
+print(a + b + c)
+
+pi = 3.1415
+print(f"O numero pi com duas casas decimais e {pi:.2f}")
+
+print(8 == "8") 
+
+# teste = int(input("lalala = "))
+# print(teste)
